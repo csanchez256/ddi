d368be4b3b75c7b3bf730abe71ad6c30d86857bd
Chris <css7c3@gmail.com>
2018-11-18 20:56:54 -0700

    initial commit

---
+++ resources/javascript/quoteGenerator.js
@@ -0,0 +1,8 @@
+var quotes = [
+  ' \" A bird sitting on a tree branch is never <br> afraid of the branch breaking, because <br> her trust is not on the branch, but on <br> her own wings. \" -? ',
+  ' \" Find what you love and let it kill you. "\ -Charles Bukowski ',
+  ' \" If you wait for inspiration to write you\'re not a writer, <br> you\'re a waiter. "\ -Dan Poynter',
+  ' \" Genius is the ability to put into effect what is on your mind. "\ -F. Scott Fitzgerald',
+  ' \" Where your talents and the needs of the world cross; <br> there lies your vocation. "\ -Aristotle'
+];
+
